880fa784dd20727de7c12fa95ea3c9d0c09911d2
E9965-Kenny <e9965@nthykyldss.edu.hk>
2020-11-08 13:20:53 +0800

    Update

---
+++ Colab/ShellBox/bin/sh/auto_unzip.sh
@@ -0,0 +1,300 @@
+#!/bin/bash
+OLD_IFS=$IFS
+IFS=$(echo -en "\n\b")
+#-------------------------------------------------------------------
+[[ ${USER_OS} == 2 ]] && CONC_FLAG=0 || CONC_FLAG=2
+DRAWLINE(){
+	echo -e "${yellow}================================================================================${plain}"
+}
+UNZIP_MULTI(){
+	temp_fifo="/tmp/$$.fifo"
+	mkfifo ${temp_fifo}
+	exec 4<>${temp_fifo}
+	rm -f ${temp_fifo}
+	for ((i=0;i<${1};i++))
+	do
+    	echo >&4
+    done
+}
+CONC_PATH(){
+	a=$1
+	if [[ ${CONC_FLAG} == 0 ]]
+	then
+		a=${a#*/} && a=${a#*/}
+		disk=${a:0:1} && disk=$(echo ${disk} | tr 'a-z' 'A-Z')
+		a=${disk}:${a:1} && a=${a////\\}
+		[ -z ${2} ] && CONC_FLAG=1
+	fi
+}
+REVERS_PATH(){
+	a=${1}
+	if [[ ${CONC_FLAG} == 1 ]]
+	then
+		drive=${a:0:1} && drive=$(echo ${drive} | tr 'A-Z' 'a-z')
+		a=${a#*\\}
+		a=/cygdrive/${drive}/${a//\\//}
+		CONC_FLAG=0
+	fi
+}
+CHECK_ARC(){
+	checkarc=$(file -b ${1}) && checkarc=${checkarc%%\ *}
+	case ${checkarc} in
+		RAR|rar|Rar|7-zip|7-Zip|7-Z|7-z|7z|7Z|7-ZIP|Zip|ZIP)
+		return 1
+			;;
+		*)
+		return 0
+			;;
+	esac
+}
+CHECKINPUT(){
+if [[ $1 == 1 ]]
+then
+	INPUT_DIR=${TEMP_DOWN_PATH}
+else
+	if [[ ${INPUT_DIR} == "FLASE" ]]
+	then
+		echo -e "${blue}[INFO]${plain}填寫壓縮包目錄實現本地自動解壓 - 支持拖動 - 不可留空"
+		while true
+		do
+			echo -en "${yellow}[INPUT]${plain}請輸入你的${green}壓縮包的目錄${plain}:"
+			read -r INPUT_DIR
+			if [[ -d ${INPUT_DIR} ]]
+			then
+				echo -e "${green}[INFO]${plain}成功設置為$(echo ${INPUT_DIR}| tr '\\' '/')"
+				break
+			else
+				echo -ne "${sound}"
+				echo -e "${red}[INFO]${plain}請輸入正確的目錄"
+			fi
+		done
+	fi
+fi
+}
+UNZIP_TITLE(){
+DRAWLINE
+echo -e "自動解壓插件 Ver.0.0.1 By:GetIntoBus"
+DRAWLINE
+}
+ENSURE_PATH(){
+	mkdir -p ${TEMP_UNZIP_PATH} > /dev/null 2>&1
+	mkdir -p ${OUT_DIR} > /dev/null 2>&1
+}
+UNZIP_INITIALIZED(){
+	UNZIP_TITLE
+	echo -e "${blue}[INFO]${plain}初始化中......"
+	DRAWLINE
+	CHECKINPUT $1
+	echo -e "${blue}[INFO]${plain}完成初始化"
+	DRAWLINE
+}
+SET_TEMP_FILE_LIST(){
+	rm -rf ${TEMP_FILE_LIST} > /dev/null 2>&1
+	touch ${TEMP_FILE_LIST} > /dev/null 2>&1
+}
+SET_FAIL_FILE_LIST(){
+	rm -rf ${FAIL_FILE_LIST} > /dev/null 2>&1
+	touch ${FAIL_FILE_LIST} > /dev/null 2>&1
+}
+SET_SUCC_FILE_LIST(){
+	rm -rf ${SUCC_FILE_LIST} > /dev/null 2>&1
+	touch ${SUCC_FILE_LIST} > /dev/null 2>&1
+}
+DEL_BLANK_FOLDER(){
+	find ${TEMP_PATH} -type d -empty -delete
+}
+COLAB_TEMP_MV(){
+    if [[ ${USER_OS} == 1 ]]
+    then
+        touch ${TEMP_FILE_LIST}.tmp
+        for i in $(cat ${TEMP_FILE_LIST})
+        do
+            rsync --remove-source-files --info=progress2 --no-inc-recursive -zrI "${i}" "${TEMP_DOWN_PATH}/${i##*\/}"
+            echo ${TEMP_DOWN_PATH}/${i##*\/} >> ${TEMP_FILE_LIST}.tmp
+        done
+        rm -rf ${TEMP_FILE_LIST}
+        mv ${TEMP_FILE_LIST}.tmp ${TEMP_FILE_LIST}
+    fi
+}
+CHECK_TEMP_DIR(){
+	if [ ! -d  ${TEMP_DOWN_PATH} ]
+	then
+		mkdir -p ${TEMP_DOWN_PATH}
+	fi
+	if [ ! -d ${TEMP_UNZIP_PATH} ]
+	then
+		mkdir -p ${TEMP_UNZIP_PATH}
+	fi
+}
+METHOD(){
+	echo -e -n "${yellow}[INFO]${plain}是否仅提取文件/无文件夹结构[默认为Y|输入任意字符取消]:"
+	read ex
+	if [ -z ${ex} ]
+	then
+		ex=e
+	else
+		ex=x
+	fi
+}
+#-------------------------------------------------------------------
+NUM_RUN=0
+FILE_NUM=0
+TEMP_FILE_LIST=${TEMP_PATH}/list.txt
+FAIL_FILE_LIST=${TEMP_PATH}/fail_list.txt
+SUCC_FILE_LIST=${TEMP_PATH}/succ_list.txt
+DUMMY="."
+TRY_PASS=""
+UNZIP_INITIALIZED $1
+SET_SUCC_FILE_LIST
+CHECK_TEMP_DIR
+[ -z ${ex} ] && METHOD
+clear
+#-------------------------------------------------------------------
+while true
+do
+	let "NUM_RUN++"
+	echo -e "${green}[INFO]${plain}開始第[ ${NUM_RUN} ]輪的解壓"
+	DRAWLINE
+	ENSURE_PATH
+	echo -e "${yellow}[INFO]${plain}正在尋找壓縮文件中......"
+	CHECKFILES_LIST=$(find ${INPUT_DIR} -type f -name "*" )
+	UNZIP_MULTI 256
+	wait
+	SET_TEMP_FILE_LIST
+	SET_FAIL_FILE_LIST
+	for i in ${CHECKFILES_LIST}
+	do
+		read -u4
+		{
+			CHECK_ARC $i
+			if [[ $? == 1 ]]
+			then
+				echo "$i" >> ${TEMP_FILE_LIST}
+			fi
+			echo >&4
+		}&
+	done
+	wait
+	exec 4>&-
+	if [[ ${NUM_RUN} > 1 ]]
+	then
+		CHECKFILES_LIST=$(find ${TEMP_UNZIP_PATH} -type f -name "*" )
+		UNZIP_MULTI 256
+		wait
+		for i in ${CHECKFILES_LIST}
+		do
+			read -u4
+			{
+				CHECK_ARC $i
+				if [[ $? == 1 ]]
+				then
+					echo "$i" >> ${TEMP_FILE_LIST}
+				fi
+				echo >&4
+			}&
+		done
+		wait
+		exec 4>&-
+	fi
+	echo -e "${green}[INFO]${plain}完成尋找壓縮文件"
+	echo -e "${yellow}[INFO]${plain}正在處理壓縮文件中......"
+	COLAB_TEMP_MV
+	for i in $(cat ${TEMP_FILE_LIST}| sort -n)
+	do
+		if [[ ${DUMMY%%.*} != ${i%%.*} ]]
+		then
+			CONC_PATH ${i} 0
+			FILE_LIST[${FILE_NUM}]=${a}
+			DUMMY=${i}
+			let "FILE_NUM++"
+		fi
+	done
+	CONC_PATH ${TEMP_UNZIP_PATH} && TEMP_UNZIP_PATH=${a}
+	if [[ ${#FILE_LIST[@]} > 0 ]]
+	then
+		echo -e "${yellow}[INFO]${plain}正在解壓文件中......"
+		UNZIP_MULTI ${UNZIP_THREAD}
+		wait
+		for i in ${FILE_LIST[@]}
+		do
+			read -u4
+			{
+				[[ ${USER_OS} == 2 ]] && a=${i##*\\} || a=${i##*\/}
+				for TRY_PASS in ${PASSWD[@]}
+				do
+					7z ${ex} -y -r -bsp1 -bso0 -bse0 -aot -p${TRY_PASS} -o${TEMP_UNZIP_PATH}${a} ${i}
+					if [[ $? != 2 ]]
+					then
+						echo "[ ${i##*\/} ] | 解壓密碼: [ ${TRY_PASS} ]" >> ${SUCC_FILE_LIST}
+						break
+					else
+						if [[ ${TRY_PASS} == ${PASSWD[-1]} ]]
+						then
+							echo ${i} >> ${FAIL_FILE_LIST}
+							break
+						fi
+					fi
+				done
+				echo >&4
+			}&
+		done
+		wait
+		exec 4>&-
+	#Start The Process of The Wrong Pass Arc
+		for i in $(cat ${FAIL_FILE_LIST})
+		do
+			while true
+			do
+				echo -ne "${sound}"
+				echo -ne "${red}[INFO]${plain}錯誤密碼 | 直接回車進行跳過 | 請輸入${i##\/}的解压密码:"
+				read TRY_PASS
+				if [[  ${TRY_PASS} != "" ]]
+				then
+					7z ${ex} -y -r -aot -p${TRY_PASS} -o${TEMP_UNZIP_PATH}${a} ${i}
+					if [[ $? != 2 ]]
+					then
+						break
+					fi
+				else
+					if [[ ${OD} == 0 ]]
+					then
+						rsync --remove-source-files --info=progress2 --no-inc-recursive -zrI ${i} ${OUT_DIR}
+					else
+						rclone move ${i} OneDrive:/ -v --transfers=${MAXPARALLEL} --cache-chunk-size 16M --no-traverse --config "${RCLONE}"
+					fi
+					break
+				fi
+			done
+		done
+	#End for the The Process of The Wrong Pass Arc
+	#Remove All Succ File
+		for i in $(cat ${TEMP_FILE_LIST})
+		do
+			rm -f ${i}
+		done
+		clear
+	#End For Remove
+		unset FILE_LIST
+	else
+		echo -e "${red}[INFO]${plain}已無壓縮文檔需要解壓"
+		break
+	fi
+done
+DRAWLINE
+echo -e "${green}[INFO]${plain}以下文件解壓成功:"
+cat ${SUCC_FILE_LIST}
+DRAWLINE
+echo -e "${yellow}[INFO]${plain}正在移往解壓目錄"
+DEL_BLANK_FOLDER > /dev/null 2>&1
+REVERS_PATH ${TEMP_UNZIP_PATH} && TEMP_UNZIP_PATH=${a}
+if [[ ${OD} == 0 ]]
+then
+	rsync --remove-source-files --info=progress2 --no-inc-recursive -zrI ${TEMP_UNZIP_PATH} ${OUT_DIR}
+else
+	rclone move ${TEMP_UNZIP_PATH} OneDrive:/ -v --transfers=${MAXPARALLEL} --cache-chunk-size 16M --no-traverse --config "${RCLONE}"
+fi
+DEL_BLANK_FOLDER > /dev/null 2>&1
+wait
+echo -e "${green}[INFO]${plain}移動完成"
+#-------------------------------------------------------------------
+IFS=$OLD_IFS
---
+++ Colab/ShellBox/bin/sh/baidu_down.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+OLD_IFS=$IFS
+IFS=$(echo -en "\n\b")
+echo -e "${yellow}[INFO]${plain}获取BDSTOKEN中......"
+filetxt=${TEMP_DOWN_PATH}/list.txt
+[ ! -f ${filetxt} ] && touch ${filetxt}
+fileno=0
+[ -z ${KEYCOOKIES} ] && export KEYCOOKIES="$(echo $COOKIES | grep -oE 'BDUSS=([[:alnum:]]|[[:punct:]])+;') $(echo $COOKIES | grep -oE 'STOKEN=[[:alnum:]]+;')"
+[ -z ${BDSTOKEN} ] && export BDSTOKEN=$(curl -s --cookie "${KEYCOOKIES}" "https://pan.baidu.com/disk/home/" | grep -oE 'bdstoken[[:punct:]]+ [[:punct:]]+[[:alnum:]]+') && BDSTOKEN=${BDSTOKEN##*\'}
+[[ ${USER_OS} == 2 ]] && CONC_FLAG=0 || CONC_FLAG=2
+if [[ ${OD} == 1 ]]
+then
+	DOWN_PATH=${TEMP_DOWN_PATH}
+else
+	DOWN_PATH=$1
+fi
+clear
+#-------------------------------------------------------
+DRAWLINE(){
+	echo -e "${yellow}================================================================================${plain}"
+}
+DOWN_MULTI(){
+	temp_fifo="/tmp/$$.fifo"
+	mkfifo ${temp_fifo}
+	exec 5<>${temp_fifo}
+	rm -f ${temp_fifo}
+	for ((i=0;i<$1;i++))
+	do
+    	echo >&5
+    done
+}
+BAIDU_DIR_INI(){
+	bd mkdir ${TEMP_BAIDU_DOWN_PATH} > /dev/null 2>&1
+	bd mv ${TEMP_BAIDU_DOWN_PATH}* /
+	DRAWLINE
+}
+TRANS(){
+	link=0
+	key=0
+	echo -e -n "${green}[ 留空停止转存 ]请输入分享链接:${plain}"
+    read -r link
+    if [[ -z ${link} ]]
+    then
+    	echo -e "${yellow}[ INFO ]本次無轉存文件,即將開始下載文件....${plain}"
+    	DRAWLINE
+        return 0
+    else
+        if [[ ${link} =~ "https" ]]
+        then
+            link=${link#https\:\/\/*}
+            echo -e -n "${yellow}[ 留空停止转存 ]請輸入密碼:${plain}"
+            read -r key
+            echo -e "${red}[ INFO ]${plain}轉存中"
+            bd transfer ${link} ${key}
+        else
+            echo -e "${red}[ INFO ]${plain}轉存中"
+            BDRAPID ${link}
+        fi
+        DRAWLINE
+        return 1
+    fi
+}
+BDRAPID(){
+	IFS=" "
+	if [[ ${1} =~ "bdpan://" ]]
+	then
+		linkarr=($(echo ${1:8}| base64 -d | tr ' ' '_' |tr '|' ' '))
+		linkarr[0]=$(echo ${linkarr[3]}|hexdump -v -e '"\\" /1 "%02x"'|tr '\\' '%')
+		curl -s --cookie "${KEYCOOKIES}" -d "path=/ShellBox/${linkarr[0]}&content-length=${linkarr[1]}&content-md5=${linkarr[2]}&slice-md5=${linkarr[3]}" -X POST "https://pan.baidu.com/api/rapidupload?app_id=250528&bdstoken=${BDSTOKEN}&channel=chunlei&clienttype=0&rtype=1&web=1" > ${TEMP_PATH}/res.json
+	else
+		linkarr=($(echo ${1} | tr ' ' '_' | tr '#' ' '))
+		linkarr[3]=$(echo ${linkarr[3]}|hexdump -v -e '"\\" /1 "%02x"'|tr '\\' '%')
+		curl -s --cookie "${KEYCOOKIES}" -d "path=/ShellBox/${linkarr[3]}&content-length=${linkarr[2]}&content-md5=${linkarr[0]}&slice-md5=${linkarr[1]}" -X POST "https://pan.baidu.com/api/rapidupload?app_id=250528&bdstoken=${BDSTOKEN}&channel=chunlei&clienttype=0&rtype=1&web=1" > ${TEMP_PATH}/res.json
+	fi
+	ERR=$(cat ${TEMP_PATH}/res.json | grep -oE "\"errno\":-?[[:alnum:]]") && ERR=${ERR##*\:}
+	case $ERR in
+		0)
+			echo -e "${green}[INFO]${plain}保存成功"
+			;;
+		2)
+			echo -e "${red}[INFO]${plain}保存失败，请重新登录"
+			;;
+		6)
+			echo -e "${red}[INFO]${plain}Cookie已过期，请重新登录"
+			;;
+		4)
+			echo -e "${red}[INFO]${plain}服务器无相关文件，请检查快链"
+			;;
+		8)
+			echo -e "${red}[INFO]${plain}已有同名文件"
+			;;
+		1)
+			echo -e "${red}[INFO]${plain}容量不足"
+			;;
+		*)
+			echo -e "${red}[INFO]${plain}未知错误"
+			;;
+		esac
+		IFS=$(echo -en "\n\b")
+}
+CONC_PATH(){
+	a=$1
+	if [[ ${CONC_FLAG} == 0 ]]
+	then
+		a=${a#*/} && a=${a#*/}
+		disk=${a:0:1} && disk=$(echo ${disk} | tr 'a-z' 'A-Z')
+		a=${disk}:${a:1} && a=${a////\\}
+		CONC_FLAG=1
+	fi
+}
+REVERS_PATH(){
+	a=$1
+	if [[ ${CONC_FLAG} == 1 ]]
+	then
+		drive=${a:0:1} && drive=$(echo ${drive} | tr 'A-Z' 'a-z')
+		a=${a#*\\}
+		a=/cygdrive/${drive}/${a//\\//}
+		CONC_FLAG=0
+	fi
+}
+DOWNLOAD(){
+	rm -rf ${filetxt} > /dev/null 2>&1
+	echo -e "${yellow}[INFO]${plain}正在獲取下載清單......"
+	DRAWLINE
+	bd match ${TEMP_BAIDU_DOWN_PATH}* > ${filetxt}
+	if [[ -f ${filetxt} ]]
+	then
+    	fileno=$(wc -l ${filetxt})
+    	fileno=${fileno%%\ *}
+    	while [[ ${fileno} > 0 ]]
+    	do
+    	    dummy=`expr ${fileno} - 1 `
+    	    file[${dummy}]=$(tail -${fileno} ${filetxt}|head -1)
+    	    echo -e "${green}${file[${dummy}]}${plain}"
+    	    fileno=`expr ${fileno} - 1 `
+    	done
+    	fileno=${file[@]}
+    	echo -e -n "${red}即將下載以上文件 | 按下 <Enter> 進行確認:${plain}"
+    	read -n 1
+    	rm -f ${filetxt}
+    	DRAWLINE
+		CONC_PATH ${DOWN_PATH} && DOWN_PATH=${a}
+    	for i in ${file[*]}
+    	do
+    	    read -u5
+        	{
+        	    bd d --nocheck -mode locate --ow --retry 10 --saveto "${DOWN_PATH}" ${i}
+        	    echo >&5
+        	}&
+    	done
+    	wait
+		exec 5>&-
+    	for i in ${file[*]}
+    	do
+	    	{
+	    	    bd rm ${i}
+	    	}&
+    	done
+    	wait
+    	DRAWLINE
+    	echo -e "${blue} 下载完成${plain}"
+    	DRAWLINE
+	else
+	    echo -e "${red}| 获取失败 | 請回報Bug |${plain}"
+	    DRAWLINE
+	    sleep 2s && exit 2
+	fi
+	[[ ${OD} == 1 ]] && [[ ${CHOICE} == 2 ]] && rclone move ${DOWN_PATH} OneDrive:/ -v --transfers=${MAXPARALLEL} --cache-chunk-size 16M --no-traverse --config "${RCLONE}"
+}
+BAIDU_DOWN_TITLE(){
+echo -e "${yellow} BaiduPCS-Go 辅助下载插件 ver0.3.0 | By:GetIntoBus ${plain}"
+DRAWLINE
+echo -e "${yellow} 正在初始化 ${plain}"
+DRAWLINE
+}
+#-------------------------------------------------------
+BAIDU_DOWN_TITLE
+DOWN_MULTI ${MAXPARALLEL}
+BAIDU_DIR_INI
+while [[ true ]]
+do
+	TRANS
+	if [[ $? == 0 ]]
+	then
+		break
+	fi
+done
+DOWNLOAD
+REVERS_PATH ${DOWN_PATH} && DOWN_PATH=${a}
+IFS=$OLD_IFS
