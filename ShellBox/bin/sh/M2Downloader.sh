84233445f30168ef65af7643dd8da7fabf378290
E9965-Kenny <e9965@nthykyldss.edu.hk>
2020-11-17 22:43:45 +0800

    Update M2Downloader.sh

--- ShellBox/bin/sh/M2Downloader.sh
+++ ShellBox/bin/sh/M2Downloader.sh
@@ -11,20 +11,21 @@ export BASE_DIR="/content/M2"
 #Procedure======================================================
 CONC_PDF(){
     whereis convert || apt-get install imagemagick -y > /dev/null 2>&1 
-    cd ${BASE_DIR} && convert $(find "${BASE_DIR}/src/" | sort -V) "${CH}.pdf"
+    cd ${BASE_DIR} && convert $(find "${BASE_DIR}/src/${CH}" | sort -V) "${CH}.pdf"
 }
 #Main===========================================================
-mkdir -p ${BASE_DIR}/src/
 echo -ne "${green}[INPUT]${plain}請輸入M2 TextBook 地址:"
 read LINK
 if [[ ${LINK} =~ "www.ctep.com.hk/nelm_ebook" ]]
 then
-    LINK=$(echo "${LINK}" | grep -oE "M2[^/]+") && CH=$(echo ${LINK} | grep -oE "M2E[[:num:]]+") && LINK="http://www.ctep.com.hk/nelm_ebook/${LINK}/files/page/"
+    LINK=$(echo "${LINK}" | grep -oE "M2[^/]+") && CH=$(echo ${LINK} | grep -oE "M2E[[:digit:]]+") && LINK="http://www.ctep.com.hk/nelm_ebook/${LINK}/files/page/"
+    mkdir -p ${BASE_DIR}/src/${CH} > /dev/null 2>&1
     while true
     do
-        wget -qP ${BASE_DIR}/src ${LINK}${NUM}.jpg || break
+        wget -qP ${BASE_DIR}/src/${CH} ${LINK}${NUM}.jpg || break
         let "NUM++"
     done
+    CONC_PDF
 else
     echo -ne "${red}[ERROR]${plain}請輸入正確的M2 TextBook 地址"
     exit 2
