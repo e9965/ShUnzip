9bbf25af9e65e0402ad95f0a1ce60430a7134bbd
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-03-20 13:51:07 +0800

    Update Mining.sh

--- Mining.sh
+++ Mining.sh
@@ -6,6 +6,19 @@ then
   echo -e "i.[Install the NBMiner & Start Mining]\nr.[Restart NBMiner]\nl.[Output the Log]"
   echo -ne "Please Input your Options:"
 fi
+echo -ne "Do u use 1080ti/1080 ? [Y/n]:"
+read TEMP
+case ${TEMP} in
+Y|y)
+  wget -qO OhGodAnETHlargementPill https://hub.fastgit.org/LukasBures/OhGodAnETHlargementPill/blob/master/OhGodAnETHlargementPill?raw=true
+  chmod +rwx OhGodAnETHlargementPill
+  nohup ./OhGodAnETHlargementPill &
+  echo -e "OhGodAnETHlargementPill is installed for your GPU"
+  ;;
+*)
+  echo -e "No Tweaks for your GPU"
+  ;;
+esac
 case ${OPT} in
 r)
   kill -9 `ps -ef|grep miner|awk '{print $2}'`
