279a99fb78960202ca0fd63d84e5d98cbe5b8dd0
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-03-20 14:11:47 +0800

    Update Mining.sh

--- Mining.sh
+++ Mining.sh
@@ -1,12 +1,4 @@
-OPT=$1
-echo -ne "Please Input the Name of the SubAccount:"
-read SUB
-if [[ ${OPT} == "" ]]
-then
-  echo -e "i.[Install the NBMiner & Start Mining]\nr.[Restart NBMiner]\nl.[Output the Log]"
-  echo -ne "Please Input your Options:"
-fi
-read OPT
+TWEAK(){
 echo -ne "Do u use 1080ti/1080 ? [Y/n]:"
 read TEMP
 case ${TEMP} in
@@ -20,6 +12,17 @@ Y|y)
   echo -e "No Tweaks for your GPU"
   ;;
 esac
+}
+#=================================
+OPT=$1
+echo -ne "Please Input the Name of the SubAccount:"
+read SUB
+if [[ ${OPT} == "" ]]
+then
+  echo -e "i.[Install the NBMiner & Start Mining]\nr.[Restart NBMiner]\nl.[Output the Log]"
+  echo -ne "Please Input your Options:"
+fi
+read OPT
 case ${OPT} in
 r)
   kill -9 `ps -ef|grep miner|awk '{print $2}'`
@@ -31,6 +34,7 @@ l)
   ;;
 i)
   apt update -y && apt install cron -y
+  systemctl > /dev/null 2>&1 && INSTALL_TWEAK
   echo "0 * * * * >/root/NBMiner_Linux/nohup.out" > /etc/crontab
   /etc/init.d/crond restart
   wget --no-check-certificate https://hub.fastgit.org/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz && tar -xvf NBMiner_36.1_Linux.tgz && rm -rf NBMiner_36.1_Linux.tgz &&cd NBMiner_Linux && chmod +rwx nbminer && nohup ./nbminer -a ethash -o stratum+tcp://en.huobipool.com:1800 -u 0x7bb3ba2abe1de9b6d95697aa78b14326986ae540.${SUB} &
