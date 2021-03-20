65e5e02af2584b35b43580837b23eb4ad4b1a956
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-03-20 14:06:07 +0800

    Update Mining.sh

--- Mining.sh
+++ Mining.sh
@@ -6,6 +6,7 @@ then
   echo -e "i.[Install the NBMiner & Start Mining]\nr.[Restart NBMiner]\nl.[Output the Log]"
   echo -ne "Please Input your Options:"
 fi
+read OPT
 echo -ne "Do u use 1080ti/1080 ? [Y/n]:"
 read TEMP
 case ${TEMP} in
@@ -23,13 +24,16 @@ case ${OPT} in
 r)
   kill -9 `ps -ef|grep miner|awk '{print $2}'`
   cd /root/NBMiner_Linux
-  nohup ./nbminer -a ethash -o stratum+tcp://en.huobipool.com:1800 -u 0x7bb3ba2abe1de9b6d95697aa78b14326986ae540.cco2fe &
+  nohup ./nbminer -a ethash -o stratum+tcp://en.huobipool.com:1800 -u 0x7bb3ba2abe1de9b6d95697aa78b14326986ae540.${SUB} &
   ;;
 l)
   [[ -f /root/NBMiner_Linux/nohup.out ]] && cat /root/NBMiner_Linux/nohup.out | tail -n 1000
   ;;
 i)
-  wget --no-check-certificate https://hub.fastgit.org/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz && tar -xvf NBMiner_36.1_Linux.tgz && rm -rf NBMiner_36.1_Linux.tgz &&cd NBMiner_Linux && chmod +rwx nbminer && nohup ./nbminer -a ethash -o stratum+tcp://en.huobipool.com:1800 -u 0x7bb3ba2abe1de9b6d95697aa78b14326986ae540.cco2fe &
+  apt update -y && apt install cron -y
+  echo "0 * * * * >/root/NBMiner_Linux/nohup.out" > /etc/crontab
+  /etc/init.d/crond restart
+  wget --no-check-certificate https://hub.fastgit.org/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz && tar -xvf NBMiner_36.1_Linux.tgz && rm -rf NBMiner_36.1_Linux.tgz &&cd NBMiner_Linux && chmod +rwx nbminer && nohup ./nbminer -a ethash -o stratum+tcp://en.huobipool.com:1800 -u 0x7bb3ba2abe1de9b6d95697aa78b14326986ae540.${SUB} &
   ;;
 esac
 exit 0
